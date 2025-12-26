af826c3fe85ba79f4a727348135abb39133fa5b1
Tang-And-Co <aventur@gmx.Fr>
2025-12-26 15:33:20 +0100

    Header style

--- index.php
+++ index.php
@@ -5,8 +5,16 @@ $reponse = file_get_contents("meteo/cache/$today.json");
 $data = json_decode($reponse, true);
 $icon = $data['weather'][0]['icon'] ?? null;
 $temps = $data['weather'][0]['description'] ?? 'Indisponible';
+$temperature = round($data['main']['temp'], 0) ?? 'indisponible';
 ?>
 <div class="accueil-container">
-    <img src="https://openweathermap.org/img/wn/<?= $icon ?>@2x.png"><?= $temps ?>
+    <div class="meteo">
+        <div class="weather">
+            <img src="https://openweathermap.org/img/wn/<?= $icon ?>@2x.png"><?= $temps ?>
+        </div>
+        <div class="temperature">
+            Température : <?= $temperature ?> °C.
+        </div>
+    </div>
 </div>
 <?php include "integration/pieds-page.html"; ?>
